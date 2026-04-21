aec5fb9a930f964304939fbe892413631f193a93
Moritz Halbritter <moritz.halbritter@broadcom.com>
2026-04-21 11:05:24 +0200

    Only register the convention beans instead of overwriting the whole Micrometer metrics beans

--- shared/src/main/java/com/example/shared/OpenTelemetryConfiguration.java
+++ shared/src/main/java/com/example/shared/OpenTelemetryConfiguration.java
@@ -1,16 +1,10 @@
 package com.example.shared;
 
-import java.util.List;
-
 import io.micrometer.core.instrument.Tags;
-import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics;
-import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics;
-import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics;
 import io.micrometer.core.instrument.binder.jvm.convention.otel.OpenTelemetryJvmClassLoadingMeterConventions;
 import io.micrometer.core.instrument.binder.jvm.convention.otel.OpenTelemetryJvmCpuMeterConventions;
 import io.micrometer.core.instrument.binder.jvm.convention.otel.OpenTelemetryJvmMemoryMeterConventions;
 import io.micrometer.core.instrument.binder.jvm.convention.otel.OpenTelemetryJvmThreadMeterConventions;
-import io.micrometer.core.instrument.binder.system.ProcessorMetrics;
 import io.opentelemetry.api.OpenTelemetry;
 
 import org.springframework.context.annotation.Bean;
@@ -36,23 +30,18 @@ public class OpenTelemetryConfiguration {
     }
 
     @Bean
-    ProcessorMetrics processorMetrics() {
-        return new ProcessorMetrics(List.of(), new OpenTelemetryJvmCpuMeterConventions(Tags.empty()));
+    OpenTelemetryJvmMemoryMeterConventions openTelemetryJvmMemoryMeterConventions() {
+        return new OpenTelemetryJvmMemoryMeterConventions(Tags.empty());
     }
 
     @Bean
-    JvmMemoryMetrics jvmMemoryMetrics() {
-        return new JvmMemoryMetrics(List.of(), new OpenTelemetryJvmMemoryMeterConventions(Tags.empty()));
+    OpenTelemetryJvmThreadMeterConventions openTelemetryJvmThreadMeterConventions() {
+        return new OpenTelemetryJvmThreadMeterConventions(Tags.empty());
     }
 
     @Bean
-    JvmThreadMetrics jvmThreadMetrics() {
-        return new JvmThreadMetrics(List.of(), new OpenTelemetryJvmThreadMeterConventions(Tags.empty()));
-    }
-
-    @Bean
-    ClassLoaderMetrics classLoaderMetrics() {
-        return new ClassLoaderMetrics(new OpenTelemetryJvmClassLoadingMeterConventions());
+    OpenTelemetryJvmClassLoadingMeterConventions openTelemetryJvmClassLoadingMeterConventions() {
+        return new OpenTelemetryJvmClassLoadingMeterConventions();
     }
 
 }
